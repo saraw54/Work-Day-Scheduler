aed36fdb7999ed22fbb263e46de4527d8335cd20
Sara Stewart <saraw54@gmail.com>
2022-05-15 11:40:01 -0600

    Fixed color coordination of rows, began commenting code

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,55 @@
+//sets current date and time for header and past/present/future times
 var currentDate = moment();
 console.log(currentDate);
 $("#currentDay").text(currentDate.format("dddd, MMMM Do YYYY"))
 var currentTime = currentDate.format("h:mm:ss a")
 console.log(currentTime)
+currentTime = moment().format("H");
 
+/**
+ * Gets the CSS class to use for coloring a row
+ * @param rowTime - the hour that the colored row represents 0-23
+ * @returns past|present|future (these are CSS class names)
+ */
 function colorRow(rowTime) {
-    var taskNow = moment(currentTime, "ha");
-    // var taskEntry = moment(time, "H A");
-    if (taskNow.isBefore(rowTime) === true) {
+    if (currentTime > rowTime) {
         return "past";
     }
-    else if (taskNow.isAfter(rowTime) === true) {
+    else if (currentTime < rowTime) {
         return "future";
 
     }
-    return "present";
+    else {
+        return "present";
+    }
 }
 
+//sets times to view on calendar
+
 var timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+/**
+ * builds a div layer string for time of day 
+ * @param i -  index to the time array
+ * @returns - populates times to table
+ */
 
 function addTodBox(i) {
     return "<div id=\"" + timeArray[i] + "\" class=\"col-xs-4 col-sm-2\">" + timeArray[i] + "</div>";
 }
 
+/**
+ * builds an input box in calendar row
+ * @param i -  index to input to determine time to assign input to
+ * @returns - populates task to table
+ */
+
 function addToDo(i) {
-    var bgColor = colorRow(i);
-    //if statements goes here
+    var bgColor = colorRow(i + 9);
     return "<input id=\"" + timeArray[i] + "note\" class=\"col-xs-10 col-sm-8 " + bgColor + "\" > </input>";
 }
+
+/** */
 
 function addSaveBtn(i) {
     return "<button id=\"" + timeArray[i] + "btn\" class=\"col-xs-2 col-sm-2\" onclick=saveTask(\"" + timeArray[i] + "\")> S </button>";
@@ -72,10 +94,9 @@ function saveTask(id) {
     var saveTask = getTask();
 
     saveTask[id] = $("#" + id + "note").val();
-    // saveTask.push(addSaveBtn);
     localStorage.setItem("task", JSON.stringify(saveTask));
 }
-//executed at load time
+//executed at load time/build calender/populate input
 buildTable();
 populateTable();
 
